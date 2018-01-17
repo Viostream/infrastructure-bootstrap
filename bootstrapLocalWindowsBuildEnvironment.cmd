8330e4be35ac0c2febeaa0cd7da87cd4d1396f1c
Linden <linden.varley@viocorp.com>
2018-01-18 07:58:09 +1100

    Updated

--- bootstrapLocalWindowsBuildEnvironment.cmd
+++ bootstrapLocalWindowsBuildEnvironment.cmd
@@ -3,10 +3,8 @@ pause
 echo Enter credentials to the Viostream GitHub repository...
 set /p USERNAME= GitHub Username:
 set /p PASSWORD= GitHub Password:
-rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-rem choco install terraform -fy
-rem choco install git -fy
-mkdir tmp
-cd tmp
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install terraform -fy
+choco install git -fy
 git clone https://%USERNAME%:%PASSWORD%@github.com/Viostream/infrastructure
 rem Provision new basic AWS account and TeamCity - Ask user for input
--- downloadAndRunBootstrapScript.ps1
+++ downloadAndRunBootstrapScript.ps1
@@ -1,4 +1,4 @@
+#Do Stuff
 $BootStrapScript = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Viostream/infrastructure-bootstrap/master/bootstrapLocalWindowsBuildEnvironment.cmd'
-$BootStrapScript.Content | Out-File $env:temp\bootstrapLocalWindowsBuildEnvironment.cmd -Encoding UTF8
-$BootStrapScript.Content > $env:temp\bootstrapLocalWindowsBuildEnvironment.cmd
+$BootStrapScript.Content | Out-File $env:temp\bootstrapLocalWindowsBuildEnvironment.cmd -Encoding default
 Invoke-Expression "cmd.exe /c $env:temp\bootstrapLocalWindowsBuildEnvironment.cmd"
