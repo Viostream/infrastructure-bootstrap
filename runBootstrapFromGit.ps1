b504b698ec8d88d6d3de105e5199095bbf777f80
Linden <linden.varley@viocorp.com>
2018-01-18 06:06:41 +1100

    Initial commit

---
+++ runBootstrapFromGit.ps1
@@ -0,0 +1,17 @@
+$cred = Get-Credential
+
+$user = $cred.Username
+$pass = ConvertFrom-SecureString $cred.Password
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
+$BootStrapScript = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Viostream/infrastructure/master/bootstrap/bootstrapLocalWindowsBuildEnvironment.cmd' -Headers $Headers
+Invoke-Expression $($BootStrapScript.Content $user $pass)
