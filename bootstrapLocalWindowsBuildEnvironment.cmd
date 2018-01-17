df677a507d43a42e876a55661ead22fb026ef81c
Linden <linden.varley@viocorp.com>
2018-01-18 08:21:41 +1100

    Updated

--- bootstrapLocalWindowsBuildEnvironment.cmd
+++ bootstrapLocalWindowsBuildEnvironment.cmd
@@ -5,7 +5,10 @@ set /p PASSWORD= GitHub Password:
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install terraform -y
 choco install git -y
-git clone https://%USERNAME%:%PASSWORD%@github.com/Viostream/infrastructure
+rmdir /Q /S infrastructure
+git clone https://%USERNAME%:%PASSWORD%@github.com/Viostream/infrastructure infrastructure
 cd infrastructure
 terraform init
+echo Finished bootstrap scripts.
+pause
 rem Provision new basic AWS account and TeamCity - Ask user for input
