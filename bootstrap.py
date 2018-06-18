01e6692e8dfa3839e7eecf2b1ec5e047a644f3c8
Linden Varley <linden.varley@viocorp.net>
2018-06-18 18:33:18 +1000

    Signed-off-by: Linden Varley <linden.varley@viocorp.net>

--- bootstrap.py
+++ bootstrap.py
@@ -3,11 +3,11 @@ import subprocess
 from requests.auth import HTTPBasicAuth
 
 url = 'https://raw.githubusercontent.com/Viostream/infrastructure/master/Viostream.Pexip.Plugin.1.0.0.21.tar'
-#tarfilename = '/home/admin/Viostream.Pexip.Plugin.1.0.0.21.tar'
+tarfilename = '/home/admin/Viostream.Pexip.Plugin.1.0.0.21.tar'
 extractpath = '/home/admin/tmp'
 
-username = raw_input("Github username?")
-password = raw_input("Github password?")
+username = raw_input("Github username? ")
+password = raw_input("Github password? ")
 
 if url.find('/'):
   filename = url.rsplit('/', 1)[1]
@@ -18,7 +18,7 @@ r = requests.get(url, allow_redirects=True, auth=HTTPBasicAuth(username, passwor
 
 open(filename, 'wb').write(r.content)
 
-retcode = subprocess.call(['tar', '-xvf', filename, '-C', extractpath])
+retcode = subprocess.call(['tar', '-xvf', tarfilename, '-C', extractpath])
 if retcode == 0:
     print "Extracted successfully"
 else:
